2a3b32cf7d925ac0c837c33ac339cb46e39f6626
jlbeltran94 <jlbeltran94@gmail.com>
2017-10-07 20:00:36 -0500

    Proyecto base

---
+++ app/src/main/java/jlbeltran94/lightapp/net/ApiClient.kt
@@ -0,0 +1,33 @@
+package jlbeltran94.lightapp.net
+
+import io.reactivex.schedulers.Schedulers
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by jlbeltran94 on 7/10/17.
+ */
+object ApiClient{
+
+    val BASE_URL: String = "http://"
+    var retrofit: Retrofit ?= null
+
+
+    private fun createInstance():Retrofit{
+        if (retrofit == null){
+            retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.createWithScheduler(Schedulers.io()))
+                    .build()
+        }
+
+        return retrofit!!
+    }
+
+    fun  getInstance(): ApiInterface{
+        return createInstance().create(ApiInterface::class.java)
+    }
+
+}
---
+++ app/src/main/java/jlbeltran94/lightapp/net/ApiInterface.kt
@@ -0,0 +1,6 @@
+package jlbeltran94.lightapp.net
+
+/**
+ * Created by jlbeltran94 on 7/10/17.
+ */
+interface ApiInterface
